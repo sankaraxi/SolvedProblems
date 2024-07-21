05ea3ac7b0e6456663fe05b39818ca0c83e44c21
sankaraxi <sankarfrompalani@gmail.com>
2024-07-21 15:50:44 +0530

    solved sorted and rotated

---
+++ Arrays/src/easyProblems/SortedAndRotated.java
@@ -0,0 +1,18 @@
+package easyProblems;
+//https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+public class SortedAndRotated {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+        System.out.println(check(arr));
+    }
+    static boolean check(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] > nums[(i+1)%nums.length]){
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+}
