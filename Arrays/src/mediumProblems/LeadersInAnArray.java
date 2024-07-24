db31386e444c67a5f38e8cd33d7fe4fc877d15b8
sankaraxi <sankarfrompalani@gmail.com>
2024-07-24 10:31:34 +0530

    solved leaders in array

--- Arrays/src/mediumProblems/LeadersInAnArray.java
+++ Arrays/src/mediumProblems/LeadersInAnArray.java
@@ -1,5 +1,5 @@
 package mediumProblems;
-
+//https://www.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=leaders-in-an-array
 import java.util.ArrayList;
 
 public class LeadersInAnArray {
---
+++ Arrays/src/mediumProblems/LongestConsecutiveSequence.java
@@ -0,0 +1,13 @@
+package mediumProblems;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] arr = {100,4,200,1,3,2};
+
+        System.out.println(longestConsecutive(arr));
+    }
+
+    static int longestConsecutive(int[] nums) {
+
+    }
+}
