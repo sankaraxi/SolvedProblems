c0614a544b44f908e3838af79b47673c05486a7f
sankaraxi <sankarfrompalani@gmail.com>
2024-07-27 21:00:50 +0530

    solved merge sorted array

--- Arrays/src/hardProblems/MergeSortedArray.java
+++ Arrays/src/hardProblems/MergeSortedArray.java
@@ -46,7 +46,7 @@ public class MergeSortedArray {
         Arrays.sort(nums1);
     }
 }
-// another solution
+// another solution`
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1;
