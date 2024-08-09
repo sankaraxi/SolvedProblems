88386c576ff6b65c2b3778bc76e6a282c1398c99
sankaraxi <sankarfrompalani@gmail.com>
2024-08-09 21:32:49 +0530

    Solved RemovesHead

--- Linked List/src/easyProblems/deletion/RemovesHead.java
+++ Linked List/src/easyProblems/deletion/RemovesHead.java
@@ -5,15 +5,20 @@ public class RemovesHead {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         Node head = convertArrayToLinkedList(arr);
-
+        head = removeHead(head);
         Node temp = head;
-        int size = 0;
         while (temp != null){
-            size++;
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
-        System.out.println(size);
     }
+
+    static Node removeHead(Node head){
+        if (head == null) return null;
+        head = head.next;
+        return head;
+    }
+
     static Node convertArrayToLinkedList(int[] arr){
         Node head = new Node(arr[0]);
         Node mover = head;
