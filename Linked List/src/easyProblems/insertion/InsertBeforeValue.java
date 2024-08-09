dc4a10cc50ee94c03729777a9f3d1dee1cd883ee
sankaraxi <sankarfrompalani@gmail.com>
2024-08-10 00:05:12 +0530

    Solved InsertionProblems

---
+++ Linked List/src/easyProblems/insertion/InsertBeforeValue.java
@@ -0,0 +1,48 @@
+package easyProblems.insertion;
+
+public class InsertBeforeValue {
+    public static void main(String[] args) {
+        int[] arr = {15,45,20,22,33,43,54};
+        Node head = convertArrayToLinkedList(arr);
+        head = insertBeforeValue(head,21, 22);
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+    static Node insertBeforeValue(Node head, int insertVal, int value){
+        if (head == null) return null; // if the linked list is empty , inserting before value is technically not possible
+
+        if (head.data == value){
+            Node temp = new Node(insertVal, head);
+            return temp;
+        }
+
+        Node temp = head;
+        Node prev = null;
+        while (temp != null){
+            if (temp.data == value){
+                Node newNode = new Node(insertVal);
+                newNode.next = temp;
+                prev.next = newNode;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    static Node convertArrayToLinkedList(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+}
---
+++ Linked List/src/easyProblems/insertion/InsertTail.java
@@ -0,0 +1,37 @@
+package easyProblems.insertion;
+
+public class InsertTail {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        Node head = convertArrayToLinkedList(arr);
+        head = insertTail(head,10);
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+    static Node insertTail(Node head, int val){
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next; // this will take the temp to the last node
+        }
+        Node newNode = new Node(val); // create a new node
+        temp.next = newNode; // add the new node to the last node
+        return head;
+    }
+
+
+
+    static Node convertArrayToLinkedList(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+}
