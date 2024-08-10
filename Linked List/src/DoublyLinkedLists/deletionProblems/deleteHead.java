164541dbf3690608d00bb0707360f34cfad38814
sankaraxi <sankarfrompalani@gmail.com>
2024-08-10 19:33:02 +0530

    Solved DLLTraversals

---
+++ Linked List/src/DoublyLinkedLists/deletionProblems/deleteHead.java
@@ -0,0 +1,42 @@
+package DoublyLinkedLists.deletionProblems;
+
+public class deleteHead {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        Node head = createDoublyLinkedList(arr);
+
+        head = deleteHead(head);
+
+        Node temp = head;
+
+        while (temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+    }
+
+    static Node deleteHead(Node head){
+        if (head == null || head.next == null){
+            return null;
+        }
+        Node prev = head;
+
+        head = head.next;
+        head.prev = null;
+        prev.next = null;
+        return head;
+    }
+
+    static Node createDoublyLinkedList(int[] arr){
+        Node head = new Node(arr[0]);
+        Node prev = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i],null,prev);
+            prev.next = temp;
+            prev = prev.next;
+        }
+        return  head;
+    }
+
+}
